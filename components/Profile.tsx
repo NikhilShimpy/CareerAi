0873c8689e98994222f8779f851755efe453f849
NikhilShimpy <nikhilshimpy712@gmail.com>
2025-12-11 22:58:11 +0530

    update devloped by section

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -9,7 +9,7 @@ interface ProfileProps {
   onLogout: () => void;
 }
 
-// onsistent Avatar Set
+// Consistent Avatar Set
 const AVATARS = [
   "https://res.cloudinary.com/dtarhtz5w/image/upload/v1765317833/image4_nfbdvk.png",
   "https://res.cloudinary.com/dtarhtz5w/image/upload/v1765317832/image10_prpun0.jpg",
