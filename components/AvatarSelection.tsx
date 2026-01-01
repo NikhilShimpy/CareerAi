133ca81b3806ea82e3bb057b2ada75cedf1e9f3d
NikhilShimpy <nikhilshimpy712@gmail.com>
2026-01-01 23:38:36 +0530

    update comments

--- components/AvatarSelection.tsx
+++ components/AvatarSelection.tsx
@@ -3,7 +3,7 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from '../services/firebase';
 
   
-// Using consistent 
+// Using 
 const AVATARS = [
   "https://res.cloudinary.com/dtarhtz5w/image/upload/v1765317833/image4_nfbdvk.png",
   "https://res.cloudinary.com/dtarhtz5w/image/upload/v1765317832/image10_prpun0.jpg",
