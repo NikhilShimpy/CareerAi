ce90716179db4d933fe55df1d6379abbc9a2134f
NikhilShimpy <nikhilshimpy712@gmail.com>
2026-01-04 23:51:47 +0530

    update comments

--- components/AvatarSelection.tsx
+++ components/AvatarSelection.tsx
@@ -4,7 +4,6 @@ import { db } from '../services/firebase';
 
   
 
-// Using consistent high-quality 3D avatar assets for premium feel
 const AVATARS = [
   "https://res.cloudinary.com/dtarhtz5w/image/upload/v1765317833/image4_nfbdvk.png",
   "https://res.cloudinary.com/dtarhtz5w/image/upload/v1765317832/image10_prpun0.jpg",
